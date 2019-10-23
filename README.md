c82e88b77c1d62eef595e4ea7ed3535ea31da08a
Joe Williams <joewilliams91@hotmail.co.uk>
2019-10-23 13:30:14 +0100

    changes

---
+++ README.md
@@ -0,0 +1,9 @@
+### Web Scraper
+
+## A command line tool which takes a company email address as an input, and returns an object containing company name, company website, relevant email addresses, relevant telephone numbers, company address and relevant links (where this information is available on the website). 
+
+- Axios for fetching data;
+- Cheerio for parsing data;
+- Knwl for parsing phone numbers and email addresses;
+- GoogleMaps API for obtaining full company addresses.
+
--- utils/places.js
+++ utils/places.js
@@ -11,14 +11,22 @@ function Places(knwlInstance) {
     let results = [];
 
     for (let i = 0; i < words.length; i++) {
+
+      // two regexs for if the postcode is split up into two separate components
       const firstPostcodeRegExp = /[a-z]{1,2}\d{1}\w?/g;
       const secondPostcodeRegExp = /\d{1}[a-z]{2}/g;
+
+      // one regex for if the postcode is complete
       const fullPostcodeRegExp = /([a-z]{1,2}\d{1}\? *\d{1}[a-z]{2})$/g;
+
+      // if the postcode appears as an intact postcode, all spaces are replaced and it is pushed into the array
       if (fullPostcodeRegExp.test(words[i])) {
         const newPostcode = words[i].replace(/\s/g, "");
         if (!results.includes(newPostcode)) {
           results.push(newPostcode);
         }
+
+      // if the postcode appears in two separate components, and/or is mixed with other characters, the relevant matches are concatenated and pushed into the array
       } else if (
         firstPostcodeRegExp.test(words[i]) &&
         secondPostcodeRegExp.test(words[i + 1])
